c9f916e4a819262997294324407dc25f0feea765
Aniket Singh <27aniketsingh077@gmail.com>
2026-04-13 18:15:35 +0530

    First commit

---
+++ server/src/config/config.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+if(!process.env.MONGODB_URI){
+  throw new Error("MONGODB_URI is not defined in env");
+}
+
+const config = {
+        MONGODB_URI: process.env.MONGODB_URI
+}
+
+
+export default config;
